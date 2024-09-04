335b1d251c3bdd1158266c7b3450f8ac175442f6
felixojiambo <felixojiamboe@gmail.com>
2024-09-04 09:09:42 +0300

    models definition

---
+++ src/main/java/com/zep/dzenshops/DzenshopsApplication.java
@@ -0,0 +1,13 @@
+package com.zep.dzenshops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DzenshopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DzenshopsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zep/dzenshops/DzenshopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zep.dzenshops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DzenshopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
